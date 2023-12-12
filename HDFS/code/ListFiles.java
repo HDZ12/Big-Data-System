03cf23d71180d72acba7a918c9dbe4b642d2d787
HDZ <99587726+HDZ12@users.noreply.github.com>
2023-12-12 12:18:24 +0000

    Create ListFiles.java

---
+++ HDFS/code/ListFiles.java
@@ -0,0 +1,7 @@
+import java.io.IOException
+import java.net.URI
+import java.net.URISyntaxException
+import org.apache.hadoop.conf.confirguation
+import org.apache.hadoop.fs.FileSystem
+import org.apache.hadoop.fs.Path
+import org.apache.hadoop.fs.FileStatus
