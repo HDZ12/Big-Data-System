46a6cb22cbf97d8f52afd32aad5af6084d95ed45
HDZ <99587726+HDZ12@users.noreply.github.com>
2023-12-12 12:09:22 +0000

    Create CopyFromLocalFile.java

---
+++ HDFS/code/CopyFromLocalFile.java
@@ -0,0 +1,17 @@
+import java.io.IOException
+import java.net.URI
+import java.net.URISyntaxException
+import org.apache.hadoop.conf.confirguation
+import org.apache.hadoop.fs.FileSystem
+import org.apache.hadoop.fs.Path
+Public class CopyFromLocalFile{
+    public static void main(String[] args) throws IOException,URISyntaxException{
+        confirguation conf = new Confirguation;
+        String hdfsPath = "hdfs://hadoop01:9000";
+        FileSystem hdfs = FileSystem.get(new URI(hdfsPath),conf);
+        String from_linux = "/export/data/hadoop4/sample_data";
+        String to_HDFS = "/hdfstest/";
+        hdfs.copyFromLocalFile(new Path(from_linux),new Path(to_HDFS));
+        System.out.println("Finish!");
+    }
+}
