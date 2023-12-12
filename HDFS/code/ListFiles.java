4f2f59fe6b640c909ed4df30583f894917a7d4c4
HDZ <99587726+HDZ12@users.noreply.github.com>
2023-12-12 12:25:11 +0000

    Update ListFiles.java

--- HDFS/code/ListFiles.java
+++ HDFS/code/ListFiles.java
@@ -5,3 +5,15 @@ import org.apache.hadoop.conf.confirguation
 import org.apache.hadoop.fs.FileSystem
 import org.apache.hadoop.fs.Path
 import org.apache.hadoop.fs.FileStatus
+public class ListFiles{
+  public static void main(String[] args) throws IOException,URISyntaxException{
+    Confirguation conf = new Confirguation();
+    String hdfsPath = "hdfs://hadoop01:9000";
+    FileSystem hdfs = FileSystem.get(new URI(hdfsPath),conf);
+    String watchHDFS = "/hdfstest";
+    FileStatus[] files = hdfs.listStatus(new Path(watchHDFS));
+    for(FileStatus file:files){
+      System.out.println(file.getPermission()+" "+file.getOwner()+" "+file.getGroup()+" "+file.getPath());
+    }
+  }
+}
