ec29f7ab3ac1826bf1551753cc64e7080e244f32
HDZ <99587726+HDZ12@users.noreply.github.com>
2023-12-12 13:10:55 +0000

    Create IteratorListFiles.java

---
+++ HDFS/code/IteratorListFiles.java
@@ -0,0 +1,20 @@
+import java.io.IOException
+import java.net.URI
+import java.net.URISyntaxException
+import org.apache.hadoop.conf.confirguation
+import org.apache.hadoop.fs.FileSystem
+import org.apache.hadoop.fs.Path
+import org.apache.hadoop.fs.FileStatus
+public class IteratorListFiles{
+  public static void main(String[] args) throws IOException{
+    confirguation conf = new Confirguation();
+    String hdfsPath = "hdfs://hadoop01:9000";
+    FileSystem hdfs = FileSystem.get(new URI(hdfsPath),conf);
+    String watchHDFS = "/";
+    IteratorListFiles(hdfs,Path(watchHDFS));
+  }
+  public static void IteratorListFiles(FileSystem hdfs,Path path) throws FileNotFoundException,IOException{
+    FileStatus[] files = hdfs.listStatus(path);
+    for(FifleStatus file:files){
+      if(file.isDirectory()){
+          System.out.println(file.getPermission()
